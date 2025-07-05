2d522510a05e7e7c9c3d062a87c9ceaee4fe8794
booleanlambda <61171334+booleanlambda@users.noreply.github.com>
2025-07-05 13:25:47 -0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-const mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN!;
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
