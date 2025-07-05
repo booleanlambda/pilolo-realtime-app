cf130602a000928405126f818de7a3cfe897aa17
booleanlambda <61171334+booleanlambda@users.noreply.github.com>
2025-07-05 12:28:41 -0400

    Create env.d.ts

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+// env.d.ts (create this in your root directory if missing)
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_SUPABASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+  }
+}
